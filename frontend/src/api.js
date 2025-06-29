49a83ea541a31d30e45e6fda346cf84048d1435c
Itonkdong <kostadinoskiviktor@yahoo.com>
2025-06-29 18:01:50 +0200

    clean version

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL: 'http://localhost:8080/api/', // Docker Compose direct backend access
-    baseURL: '/api/', // Use relative path for Kubernetes Ingress routing
+    // baseURL: 'http://localhost:8080/api/', // Direct backend access
+    baseURL: '/api/', // Use relative path for Docker and Kubernetes Ingress routing
 });
